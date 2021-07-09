a780aa4b1236edd154e8c484b9d8cf595c178095
Aviva Mondesir <avivamondesir@Avivas-MacBook.local>
2021-07-09 14:52:07 -0500

    Navbar2

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -2,6 +2,10 @@ import React from "react"
 import { Route } from "react-router-dom"
 import { Home } from "./Home"
 import { AnimalCard } from "./animal/AnimalCard"
+import { Employee } from "./employee/Employee"
+import { Customer } from "./customer/Customer"
+import { Location } from "./Location/Location"
+
 
 export const ApplicationViews = () => {
     return (
@@ -14,6 +18,21 @@ export const ApplicationViews = () => {
             {/* Render the animal list when http://localhost:3000/animals */}
             <Route path="/animals">
                 <AnimalCard />
+            </Route>
+
+             {/* Render the customer list when http://localhost:3000/animals */}
+             <Route path="/customers">
+                <Customer />
+            </Route>
+
+             {/* Render the employee list when http://localhost:3000/animals */}
+             <Route path="/employees">
+                <Employee />
+            </Route>
+
+             {/* Render the location list when http://localhost:3000/animals */}
+            <Route path="/locations">
+                <Location />
             </Route>
         </>
     )
